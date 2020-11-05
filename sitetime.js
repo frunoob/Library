cb4fcd1a310e53f7fda6ce621866b9edb0043f8f
frunoob <3024446880@qq.com>
2020-11-05 23:17:26 +0800

    0

--- sitetime.js
+++ sitetime.js
@@ -1,38 +1,3 @@
-// function siteTime(){
-//     window.setTimeout("siteTime()", 1000);
-    // var today = new Date()
-    // var todayYear = today.getFullYear()
-    // var todayMonth = today.getMonth()
-    // var todayDate = today.getDate()
-    // var todayHour = today.getHours()
-    // var todayMinute = today.getMinutes()
-    // var todaySecond = today.getSeconds()
-//     var seconds = 1000
-//     var minutes = seconds * 60
-//     var hours = minutes * 60
-//     var days = hours * 24
-//     var years = days * 365
-//     /* Date.UTC() -- 返回date对象距世界标准时间(UTC)1970年1月1日午夜之间的毫秒数(时间戳) 
-//     year - 作为date对象的年份，为4位年份值
-//     month - 0-11之间的整数，做为date对象的月份
-//     day - 1-31之间的整数，做为date对象的天数
-//     hours - 0(午夜24点)-23之间的整数，做为date对象的小时数
-//     minutes - 0-59之间的整数，做为date对象的分钟数
-//     seconds - 0-59之间的整数，做为date对象的秒数
-//     microseconds - 0-999之间的整数，做为date对象的毫秒数 */
-//     var t1 = Date.UTC(2020,10,23,06,30,55)
-//     var t2 = Date.UTC(todayYear,todayMonth,todayDate,todayHour,todayMinute,todaySecond)
-//     var diff = t2-t1
-//     var diffYears = Math.floor(diff/years)
-//     var diffDays = Math.floor((diff/days)-diffYears*365)
-//     var diffHours = Math.floor((diff-(diffYears*365+diffDays)*days)/hours)
-//     var diffMinutes = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours)/minutes)
-//     var diffSeconds = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours-diffMinutes*minutes)/seconds)
-//     /* document.getElementById("sitetime").innerHTML=" 已运行"+diffYears+" 年 "+diffDays+" 天 "+diffHours+" 小时 "+diffMinutes+" 分钟 "+diffSeconds+" 秒" */
-//     // document.getElementById("sitetime").innerHTML="很努力地存活了 "+(diffYears*365+diffDays)+" 天 "+diffHours+" 小时 "+diffMinutes+" 分钟 "+diffSeconds+" 秒"
-      
-// }
-
 function sitetime(){
 window.setTimeout("siteTime()", 1000);
 var today = new Date()
@@ -114,5 +79,3 @@ function daysToNow(year,month,day) {return (daysDurYears(year)-dayDurMonths(year
 console.log(daysToNow(2020,10,23),todayHour,todayMinute,todaySecond);
 }
 sitetime();
-
-   
