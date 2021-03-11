b2cb73de121b4b97720ff0425d9e73f6891e2253
frunoob <3024446880@qq.com>
2021-03-11 18:42:22 +0800

    by frunoob

--- blog/js/editPage.js
+++ blog/js/editPage.js
@@ -1,1 +1,1 @@
-title =document.getElementsByClassName("page-title")[0];pageUrl = "https://github.com/frunoob/frunoob.github.io/edit/hexo/source"+location.pathname + "index.md";title.innerHTML = title.textContent + "<a href="+pageUrl+" target='_blank'> <i class='fas fa-pen-alt'></a>";
+window.onload = function () {    title =document.getElementsByClassName("page-title")[0];pageUrl = "https://github.com/frunoob/frunoob.github.io/edit/hexo/source"+location.pathname + "index.md";title.innerHTML = title.textContent + "<a href="+pageUrl+" target='_blank'> <i class='fas fa-pen-alt'></a>";}
